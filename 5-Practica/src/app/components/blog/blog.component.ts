303e4f4bd3d24cc34ada262cf13f325aad89890f
Elmar Taghiyev Mamedova <elmartaguiyev@gmail.com>
2025-07-07 19:31:16 +0200

    Creo la dos áreas y el el array de JSON

---
+++ 5-Practica/src/app/components/blog/blog.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { INews } from '../../interfaces/inews.interface';
+
+@Component({
+  selector: 'app-blog',
+  imports: [],
+  templateUrl: './blog.component.html',
+  styleUrl: './blog.component.css'
+})
+export class BlogComponent {
+  arrayNews: INews[] = [
+    {titulo: 'Entrega de la práctica 3', imagen: 'https://media.istockphoto.com/id/1491196684/es/foto/empresarios-que-celebran-el-%C3%A9xito-en-una-oficina.jpg?s=612x612&w=0&k=20&c=xDtSPGJw40Mr8b8U3tO8tPLnG8XrXe_rn8KYRpjaU20=', cuerpo: 'Comenzamos el  máster con la primera entrega de la práctica 3, realizando un trabajo enfocado en HTML y CSS', fecha:'2 de junio de 2025'},
+    {titulo: 'Entrega de la práctica 4', imagen: 'https://media.istockphoto.com/id/643285752/es/foto/lo-hemos-hecho-otra-vez.jpg?s=612x612&w=0&k=20&c=uoFllI0F-_rvZF22XLyvfRVASRgQu82m03yb_nbvBkg=', cuerpo: 'Con esta 2 entrega hemos puesto a prueba vuestros conocimientos de Javascript y el manejo de el DOM', fecha:'30 de junio de 2025'},
+  ]
+  
+
+}
